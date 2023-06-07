e399e68c75bd4587deaa8208ba49aab822567816
Vladyslav Savchyn <57039458+Barero-dreep@users.noreply.github.com>
2023-06-07 02:26:18 -0400

    feat: small showcase

--- cpp/A_star_algo/main.cpp
+++ cpp/A_star_algo/main.cpp
@@ -1,50 +1,13 @@
 #include <iostream>
-#include <iomanip>
-#include <chrono>
 
 #include "a_star.h"
 
-// TODO: better to use some kind of a renderer or engine 
-//		 to draw a visualisation
-// 
-// iostream?
-// -- pros:
-//		I already know that
-//		reletevly easy
-// -- cons:
-//		kinda suck to do so.
-//  
-// raylib? 
-// -- pros:
-//		open-source
-//		easy-to-learn
-//		not that bad for portfolio
-// -- cons:
-//		I might need to pay for this :(
-//q
-// imGUI?
-// -- pros:
-//		usually used as helper for most of GameDev things
-//		good exp
-//		smth like 50% 50% between these two
-//		good base for portfolio
-// -- cons:
-//		not a good choice as it's more about ui for programmers
-//		still hard to learn
-//		
-// write my own using openGL/Vulkan?
-// -- pros:
-//		get knoweledge at low level of rendering
-//		perfect(?) base for my portfolio
-// -- cons:
-//		time-consuming, I mean a lot
-//		kinda suck, as I have 0 exp
-//		hard to learn
-//		not a big deal scince I can easily get it without any issues
-// 	
-
 int main() {
+	Map map;
+	Path path;
 
+	map.changeValueAtPoint(Point(2,1), Point(5,9));
+	displayPathOnMap(map, path.aStarAlgo(map,Point(1,1),Point(6,6)));
 
 	return 0;
 }
