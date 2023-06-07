3126856c60d5c8d2455ff9cf840ce6d34a376540
Vladyslav Savchyn <57039458+Barero-dreep@users.noreply.github.com>
2023-06-07 02:25:59 -0400

    feat: finished A*

--- cpp/A_star_algo/a_star.cpp
+++ cpp/A_star_algo/a_star.cpp
@@ -1,53 +1,155 @@
+// Done by Vladyslav Savchyn
+// on 2023-06-06
+
 #include "a_star.h"
 
 std::ostream& Map::displayMap(std::ostream& os) {
-    for (size_t i = 0; i < getHeight(); i++) {
-        for (size_t j = 0l; j < getWidth(); j++)
-            os << m_map[i][j] << " ";
-        os << std::endl;
-        for (size_t j = 0u; j < getWidth(); j++)
-            os << "  ";
-        os << std::endl;
-    }
+    size_t rows = getHeight();
+    size_t cols = getWidth();
 
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0u; j < cols; j++) {
+            switch (m_map[i][j]) 
+            {
+            default:
+                os << "W";
+                break;
+            case Constraints::nothing:
+                os << " ";
+                break;
+            case Constraints::wall:
+                os << static_cast<char>(219);
+                break;
+            case Constraints::path:
+                os << "\033[38;2;255;211;0mP\033[0m";
+                break;
+            case Constraints::start:
+                os << "\033[38;2;11;218;81mS\033[0m";
+                break;
+            case Constraints::destination:
+                os << "\033[38;2;245;00mD\033[0m";
+            }
+
+            os << ((j != cols - 1) ? "|" : "\n");
+        }
+
+        if (i != rows - 1) {
+            for (size_t j = 0u; j < cols; j++)
+                os << "- ";
+            os << '\n';
+        }
+    }
     return os;
 }
 
-std::ostream& Map::displayPathOnMap(Path& path, std::ostream& os) {
-    // TODO: insert return statement here
+Map& Map::changeValueAtPoint(const Point& point, int value) {
+    if (isValid(point)) {
+        m_map[point.row][point.column] = value;
+    }
+    return *this;
 }
 
-bool Map::isValid(const Point& p) {
-    return p.row > 0 && p.row < getHeight() && p.column > 0 && p.column < getWidth();
+// Doesn't display 1 or 2 or 3
+Map& Map::changeValueAtPoint(const Point& from, const Point& to, int value) {
+    int row = from.row;
+    int col = from.column;
+    int height = abs(to.row - row) + 1;
+    int width = abs(to.column - col) + 1;
+
+    if (isValid(from) && isValid(to)) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                m_map[row + i][col + j] = value;
+            }
+        }
+    }
+    return *this;
 }
 
-Path& Map::findPath(const Point& start, const Point& dest) {
-    return aStarAlgo(*this, start, dest);
+bool Map::isValid(const Point& p) const {
+    return p.row >= 0 && (size_t)p.row < getHeight() && p.column >= 0 && (size_t)p.column < getWidth();
 }
 
-float heuristics(Point& p1, Point& p2) {
-    return sqrt(pow(p2.row - p1.row, 2)
-        +
-        pow(p2.column - p1.column, 2));
+bool Map::isUnBlocked(const Point& p) const {
+    return m_map[p.row][p.column] != 1;
 }
 
-bool isDestination(const int row, const int column, const Point& dest) {
+Path& Path::addPoint(const Point& tmp_point) { 
+    points.push_back(tmp_point);
+    return *this;
+}
+
+Point Path::operator[](size_t i) const{
+    return i < points.size() ? points[i] : Point();
+}
+
+size_t Path::getSize() const {
+    return points.size();
+}
+
+void Path::reverse() {
+    std::reverse(points.begin(), points.end());
+}
+
+bool Path::isDestination(const int row, const int column, const Point& dest) const {
     return dest.row == row && dest.column == column;
 }
 
-Path& aStarAlgo(Map& map, const Point& start, const Point& dest) {
-    Path finalPath{};
+double Path::heuristics(const Point& p1, const Point& p2) {
+    return sqrt(pow(p2.row - p1.row, 2)
+                +
+                pow(p2.column - p1.column, 2));
+}
 
-    if(!map.isValid(start) || !map.isValid(dest)) return finalPath;
+Path& Path::tracePath(Cell cells[][MAP_COLS], const Point& start, const Point& end) {
+    Point p = end;
 
-    // if(!map.isUnblocked(start) || !map.isUnBlocked(dest)) return finalPath;
+    while (!(cells[p.row][p.column].parent == p)) {
+        this->addPoint(p);
 
-    if(start == dest) return finalPath;
+        p = cells[p.row][p.column].parent;
+    }
 
-    bool closedList[MAP_ROWS][MAP_COLS]{ false };
+    this->addPoint(start);
+    this->reverse();
+
+    return *this;
+}
+
+Path& Path::aStarAlgo(const Map& map, const Point& start, const Point& dest, bool allowAllDirs) {
+    if (!map.isValid(start)) {
+        std::cout << "Start point is not a valid point\n";
+        return *this;
+    }
+
+    if (!map.isValid(dest)) {
+        std::cout << "Destenation point is not a valid point\n";
+        return *this;
+    }
+
+    if (!map.isUnBlocked(start)) {
+        std::cout << "Start point is blocked\n";
+        return *this;
+    }
+
+    if (!map.isUnBlocked(dest)) {
+        std::cout << "Destenation point is blocked\n";
+        return *this;
+    }
+
+    if (start == dest) {
+        std::cout << "We are already at destenation\n";
+        return *this;
+    }
+      
+    // curr version needs constants run this stuff
+    bool closedList[MAP_ROWS][MAP_COLS]{false};
     Cell cells[MAP_ROWS][MAP_COLS];
-    std::set<std::pair<double, Point>> openList;
-    bool foundDest = false;
+    
+    std::vector<std::pair<double, Point>> openList;
+    
+    double moveCost = 0.0;
+    size_t steps = 0;
     int i = start.row, j = start.column;
 
     cells[i][j].f = 0.0;
@@ -56,10 +158,14 @@ Path& aStarAlgo(Map& map, const Point& start, const Point& dest) {
     cells[i][j].parent.row = i;
     cells[i][j].parent.column = j;
 
-    openList.insert(std::make_pair(0.0, Point(i,j)));
-
+    openList.emplace_back(std::make_pair(0.0, Point(i,j)));
 
     while (!openList.empty()) {
+        if (steps++ >= max_num_points) {
+            std::cout << "Reached max num of steps\n";
+            return *this;
+        }
+
         std::pair<double, Point> p = *openList.begin();
         openList.erase(openList.begin());
 
@@ -71,32 +177,30 @@ Path& aStarAlgo(Map& map, const Point& start, const Point& dest) {
 
         for (int row = -1; row <= 1; row++) {
             for (int col = -1; col <= 1; col++) {
-                // remove that if we want to look in all directions
-                // better to put here some kind of bool?
-                if ((row == 0 && col == 0) || (row != 0 && col != 0))
+                if (!allowAllDirs && ((row == 0 && col == 0) || (row != 0 && col != 0)))
                     continue;
+
+                moveCost = (row != 0 && col != 0) ? DIAGONAL_MOVE_COST : VERTICAL_MOVE_COST;
 
                 int iNew = i + row;
                 int jNew = j + col;
 
-                if (iNew >= 0 && iNew < MAP_ROWS && jNew >= 0 && jNew < MAP_COLS) {
+                if (map.isValid(Point(iNew, jNew))) {
                     if (Point(iNew, jNew) == dest) {
                         cells[iNew][jNew].parent.row = i;
                         cells[iNew][jNew].parent.column = j;
-                        foundDest = true;
-
-                        return tracePath(cells, start, dest);
+                        
+                        std::cout << "Path was found!\n";
+                        
+                        return this->tracePath(cells, start, dest);
                     }
-                    else if (closedList[iNew][jNew] == false &&
-                             map.isUnBlocked(Point(iNew, jNew))) {
-                        // put isUnblocked here
-                        gNew = cells[iNew][jNew].g + 1.0;
-                        hNew = heuristics(Point(iNew, jNew), dest);
+                    else if (closedList[iNew][jNew] == false && map.isUnBlocked(Point(iNew, jNew)))  {
+                        gNew = cells[i][j].g + moveCost;
+                        hNew = Path::heuristics(Point(iNew, jNew), dest);
                         fNew = gNew + hNew;
 
-                        if (cells[iNew][jNew].f == FLT_MAX ||
-                            cells[iNew][jNew].f > fNew) {
-                            openList.insert(std::make_pair(fNew, Point(iNew, jNew)));
+                        if (cells[iNew][jNew].f == FLT_MAX || cells[iNew][jNew].f > fNew) {
+                            openList.emplace_back(std::make_pair(fNew, Point(iNew, jNew)));
 
                             cells[iNew][jNew].g = gNew;
                             cells[iNew][jNew].h = hNew;
@@ -108,9 +212,27 @@ Path& aStarAlgo(Map& map, const Point& start, const Point& dest) {
                 }
             }
         }
-
     }
 
+    return *this;
+}
 
-    return finalPath;
+void displayPathOnMap(const Map& map, const Path& path) {
+    Map newMap(map);
+    size_t sizeOfPath = path.getSize();
+
+    // Looking if the path is valid in terms of our map
+    for (size_t i = 0; i < sizeOfPath; i++)
+        if (!map.isValid(path[i])) {
+            std::cout << "Path is invalid" << std::endl;
+            return;
+        }
+
+    // Writting an path into newMap
+    newMap.changeValueAtPoint(path[0], Constraints::start);
+    for (size_t i = 1; i < sizeOfPath - 1; i++)
+        newMap.changeValueAtPoint(path[i], Constraints::path);
+    newMap.changeValueAtPoint(path[sizeOfPath - 1], Constraints::destination);
+
+    newMap.displayMap() << std::endl;
 }
--- cpp/A_star_algo/a_star.h
+++ cpp/A_star_algo/a_star.h
@@ -1,36 +1,48 @@
+// Done by Vladyslav Savchyn
+// 2023-06-06
+
 #ifndef A_STAR_H
 #define A_STAR_H
 
-#define TWO_DIMENTION_ALGO 2
-// #define THREE_DIMENTION_ALGO 3
+#define MAP_ROWS 10u
+#define MAP_COLS 10u
 
-#define MOWMENT_COST 1
+#define VERTICAL_MOVE_COST 1.0
+#define DIAGONAL_MOVE_COST 1.41
 
-#define MAP_ROWS 25
-#define MAP_COLS 25
+enum Constraints
+{
+	nothing,
+	wall,
+	path,
+	start,
+	destination
+};
+
+#define WALL 1
+#define PATH 2
+#define START 3
+#define DEST 4
 
 #include <iostream>
-#include <iomanip>
 
 #include <vector>
-#include <set>
 
 #include <cmath>
-#include <cfloat>
+#include <float.h>
+
 #include <algorithm>
 
 struct Point {
 	int row;
 	int column;
-//#ifdef THREE_DIMENTIONS
-//	size_t z;
-//#endif
-	Point() = default;
+
+	Point() : row(-1), column(-1){}
 	Point(int i, int j) : row(i), column(j){}
 
 	bool operator==(const Point& other) const {
 		return this->row == other.row && this->column == other.column;
-	};
+	}
 };
 
 struct Cell {
@@ -43,39 +55,48 @@ struct Cell {
 	}
 };
 
-using Path = std::vector<Point>;
-// TODO: write a route class:
-// Should have:
-//		array of points
-//		finite number of points ??
-//		method addPoint
-//		operator[] overload to get points
-//		reverse method to reverse array, possible usage of STL's reverse
-//		trace path, to trace the path ??
-//
-
-
 class Map {
-	size_t height = 25;
-	size_t width = 25;
+	size_t height = MAP_ROWS;
+	size_t width = MAP_COLS;
 
-	int m_map[25][25]{ 0 };
+	char m_map[MAP_ROWS][MAP_COLS] = { '\0' };
 public:
+	Map() = default;
+	Map(const Map& other) = default;
+	Map(Map&& other) = default;
+
 	std::ostream& displayMap(std::ostream& os = std::cout);
-	std::ostream& displayPathOnMap(Path& path, std::ostream& os = std::cout);
-	int getHeight() const { return height; };
-	int getWidth()  const { return width;  };
 
-	bool isValid(const Point& p);
-	//bool isUnBlocked(const Point& p);
+	size_t getHeight() const { return height; };
+	size_t getWidth()  const { return width;  };
 
-	Path& findPath(const Point& start, const Point& dest);
+	Map& changeValueAtPoint(const Point& point, int value = 1);
+	Map& changeValueAtPoint(const Point& from, const Point& to, int value = 1);
+
+	bool isValid(const Point& p) const;
+	bool isUnBlocked(const Point& p) const;	
 };
 
-float heuristics(Point p1, Point p2);
+class Path {
+	std::vector<Point> points;
+	size_t max_num_points = MAP_ROWS * MAP_COLS;
+public:
+	Path() = default;
+	Path(const Path& other) = default;
+	Path(Path&& other) = default;
 
-bool isDestination(const int row, const int col, const Point& dest);
+	void reverse();
+	bool isDestination(const int row, const int col, const Point& dest) const;
 
-Path& aStarAlgo(Map& map, const Point& start, const Point& dest);
+	Path& addPoint(const Point& tmp_point);
+	Point operator[] (size_t i) const;
+	size_t getSize() const;
+
+	double heuristics(const Point& p1, const Point& p2);
+	Path& tracePath(Cell cells[][MAP_COLS], const Point& start, const Point& end);
+	Path& aStarAlgo(const Map& map, const Point& start, const Point& dest, bool allowAllDirs = true);
+};
+
+void displayPathOnMap(const Map& map, const Path& path);
 
 #endif
