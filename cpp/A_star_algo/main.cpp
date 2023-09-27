df2233a45bb0bd7062b2d0cade4d235db90880ab
Vladyslav Savchyn <57039458+Barero-dreep@users.noreply.github.com>
2023-09-26 21:52:39 -0400

    msc: more readable version of main file

--- cpp/A_star_algo/main.cpp
+++ cpp/A_star_algo/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "a_star.h"
 
@@ -6,8 +7,27 @@ int main() {
 	Map map;
 	Path path;
 
-	map.changeValueAtPoint(Point(2,1), Point(5,9));
-	displayPathOnMap(map, path.aStarAlgo(map,Point(1,1),Point(6,6)));
+	map.changeValueAtPoint(Point(2, 1), Point(5, 9));
+	map.changeValueAtPoint(Point(1, 2), Point(7, 40));
+	map.changeValueAtPoint(Point(15, 15), Point(30, 30));
+
+	std::cout << "Don\'t forget to make the console\'s text smaller, as there can be some artifacts \n" 
+			  << "when you try to display \"big\" test cases (like current one)\n"
+			  << "Start? (press ENTER to continue)\n";
+
+	while (std::cin.get() != '\n')
+		;
+
+	// Calculating the path and measuring the time
+	auto start = std::chrono::high_resolution_clock::now();
+	path.aStarAlgo(map, Point(1, 1), Point(30, 31));
+	auto end = std::chrono::high_resolution_clock::now();
+
+	displayPathOnMap(map, path);
+	// display current map
+	// map.displayMap();
+
+	std::cout << "Elapsed for " << std::chrono::duration<double> (end - start).count() << "s" << std::endl;
 
 	return 0;
 }
